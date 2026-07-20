8f1967eb4d6d8afeefc85238b31a6cd3551489d3
ilauan_fleal <ilauan.fleal@gmail.com>
2026-07-19 22:56:16 -0300

    Commit, para agregar um recurso interessante no jogo

--- recorde.py
+++ recorde.py
@@ -4,10 +4,10 @@ class Recorde(Turtle):
     def __init__(self):
         super().__init__()
         self.recorde = 0
-        self.recorde_mais_alto = 0
+        with open("dados.txt") as dado:
+            self.recorde_mais_alto = int(dado.read())
         self.penup()
-        self.goto(0,270)
-        self.write(f"Recorde: {self.recorde}", align="center", font=("Arial", 24, "normal"))
+        self.goto(0,270)      
         self.hideturtle()
 
 
@@ -17,7 +17,12 @@ class Recorde(Turtle):
     
     def atualizar_recorde(self):
         self.clear()
-        self.write(f"Recorde: {self.recorde}", align="center", font=("Arial", 24, "normal"))
+        self.write(f"Recorde: {self.recorde}", align="center", font=("Arial", 20, "normal"))
+        self.goto(0,240)
+        self.write(f"Recorde mais alto: {self.recorde_mais_alto}", align="center", font=("Arial", 20, "normal"))
+        self.goto(0,210)
+
+
 
 
     
@@ -26,8 +31,11 @@ class Recorde(Turtle):
     def resetar_jogo(self):
         if self.recorde > self.recorde_mais_alto:
             self.recorde_mais_alto = self.recorde
+            with open("dados.txt", "w") as dados:
+                dados.write(f"{self.recorde_mais_alto}.")
         self.recorde = 0
         self.atualizar_recorde()
+        
    
     
 
@@ -38,5 +46,6 @@ class Recorde(Turtle):
     def elevar_recorde(self):
 
         self.recorde += 1
-        self.atualizar_recorde() #Chamada do método criado adiante, dentro desse método
+        self.atualizar_recorde()
+        
     
